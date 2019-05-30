97a1d5eab6e77f39236b3d1e5cfed78b78bd1784
Louise P <cpieri@student.42.fr>
2019-05-30 22:49:38 +0200

    Modif Grace (close fd)

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 #define	CHILD	"Grace_kid.c"
-#define	STR	"#include <stdio.h>%c#include <fcntl.h>%c#define%cCHILD%c%c%s%c%c#define%cSTR%c%c%s%c%c#define%cFT%cint main() { int fd = open(%c%s%c,O_WRONLY|O_CREAT,0x755); dprintf(fd,STR,10,10,9,9,34,CHILD,34,10,9,9,34,STR,34,10,9,9,34,CHILD,34,10,10,10,9,10,10,10); return (0); }%c%c/*%c%cGrace creates a new Grace_kid.c file when replicating it.%c*/%cFT%c"
-#define	FT	int main() { int fd = open("Grace_kid.c",O_WRONLY|O_CREAT,0x755); dprintf(fd,STR,10,10,9,9,34,CHILD,34,10,9,9,34,STR,34,10,9,9,34,CHILD,34,10,10,10,9,10,10,10); return (0); }
+#define	STR	"#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define%cCHILD%c%c%s%c%c#define%cSTR%c%c%s%c%c#define%cFT%cint main() { int fd = open(%c%s%c,O_WRONLY|O_CREAT,0x755); dprintf(fd,STR,10,10,10,9,9,34,CHILD,34,10,9,9,34,STR,34,10,9,9,34,CHILD,34,10,10,10,9,10,10,10); close(fd); return (0); }%c%c/*%c%cGrace creates a new Grace_kid.c file when replicating it.%c*/%cFT%c"
+#define	FT	int main() { int fd = open("Grace_kid.c",O_WRONLY|O_CREAT,0x755); dprintf(fd,STR,10,10,10,9,9,34,CHILD,34,10,9,9,34,STR,34,10,9,9,34,CHILD,34,10,10,10,9,10,10,10); close(fd); return (0); }
 
 /*
 	Grace creates a new Grace_kid.c file when replicating it.
