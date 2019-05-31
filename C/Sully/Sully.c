848f396df6ac8663cd3869e15a103f6bfc646bdb
Louise P <cpieri@student.42.fr>
2019-05-31 11:48:49 +0200

    Sully infinite :)

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-#include <fcntl.h>
+#include <stdlib.h>
 
+/*
+	Sully is a program that is written and executed 5 times by name Sully_X.
+*/
 int		main()
 {
 	int		i = 5;
-	char	*s = "#include <stdio.h>%c#include <fcntl.h>%c%cint%c%cmain()%c{%c%cint%c%ci = %d;%c%cchar%c*s = %c%s%c;%c%cint%c%cfd = open(%cSully_%d.c%c,O_WRONLY|O_CREAT,0x755);%c%c%cdprintf();%c%creturn (0);%c}%c";
-	int		fd = open(,O_WRONLY|O_CREAT,0x755);
+	char	*itself = "#include <stdio.h>%c#include <stdlib.h>%c%c/*%c%cSully is a program that is written and executed 5 times by name Sully_X.%c*/%cint%c%cmain()%c{%c%cint%c%ci = %d;%c%cchar%c*itself = %c%s%c;%c%cchar%c*name, *cmd;%c%cFILE%c*kid;%c%c%casprintf(&name,%cSully_%%d.c%c,i-1);%c%casprintf(&cmd,%cclang -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d; ./Sully_%%d;%c,i-1,i-1,i-1);%c%ckid = fopen(name,%cw%c);%c%cfprintf(kid,itself,10,10,10,10,9,10,10,9,9,10,10,9,9,9,i-1,10,9,9,34,itself,34,10,9,9,10,9,9,10,10,9,34,34,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,10,9,10,9,10,9,10,10);%c%cfclose(kid);%c%csystem(cmd);%c%cfree(name);%c%cfree(cmd);%c%ccmd = name = NULL;%c%creturn (0);%c}%c";
+	char	*name, *cmd;
+	FILE	*kid;
 
-	dprintf(fd,s,10,10,10,9,9,10,10,9,9,9,i-1,10,9,9,34,s,34,10,9,9,9,34,i-1,34,10,10,9,10,9,10,10);
+	asprintf(&name,"Sully_%d.c",i-1);
+	asprintf(&cmd,"clang -Wall -Wextra -Werror Sully_%d.c -o Sully_%d; ./Sully_%d;",i-1,i-1,i-1);
+	kid = fopen(name,"w");
+	fprintf(kid,itself,10,10,10,10,9,10,10,9,9,10,10,9,9,9,i-1,10,9,9,34,itself,34,10,9,9,10,9,9,10,10,9,34,34,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,10,9,10,9,10,9,10,10);
+	fclose(kid);
+	system(cmd);
+	free(name);
+	free(cmd);
+	cmd = name = NULL;
 	return (0);
 }
