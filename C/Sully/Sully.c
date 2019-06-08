f0f9bbb1d13e2d19e480a677cc03db45b32cc21a
Clément P <cpieri@student.42.fr>
2019-06-08 18:00:40 +0200

    Sully_5

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -7,17 +7,18 @@
 int		main()
 {
 	int		i = 5;
-	char	*itself = "#include <stdio.h>%c#include <stdlib.h>%c%c/*%c%cSully is a program that is written and executed 5 times by name Sully_X.%c*/%cint%c%cmain()%c{%c%cint%c%ci = %d;%c%cchar%c*itself = %c%s%c;%c%cchar%c*name, *compile, *cmd;%c%cFILE%c*kid;%c%c%cif (i == 0)%c%c%creturn (0);%c%casprintf(&name,%cSully_%%d.c%c,i-1);%c%casprintf(&compile,%cclang -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d;%c,i-1,i-1);%c%casprintf(&cmd,%c./Sully_%%d;%c,i-1);%c%ckid = fopen(name,%cw%c);%c%cfprintf(kid,itself,10,10,10,10,9,10,10,9,9,10,10,9,9,9,i-1,10,9,9,34,itself,34,10,9,9,10,9,9,10,10,9,10,9,9,10,9,34,34,10,9,34,34,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,10,9,10,9,10,9,10,10);%c%cfclose(kid);%c%csystem(compile);%c%csystem(cmd);%c%cfree(compile), free(name), free(cmd);%c%ccompile = cmd = name = NULL;%c%creturn (0);%c}%c";
+	char	*itself = "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$c/*%1$c%2$cSully is a program that is written and executed 5 times by name Sully_X.%1$c*/%1$cint%2$cmain()%1$c{%2$cint%2$c%2$ci = %3$d;%1$c%2$cchar%2$c*itself = %4$c%5$s%4$c;%1$c%2$cchar%2$c*name, *compile, *cmd;%1$c%2$cFILE%2$c*kid;%1$c%2$cif (i == 0)%1$c%2$c%2$creturn (0);%1$c%2$ci += (fopen(%4$cSully_5.c%4$c, %4$cr%4$c) == NULL) ? 0 : -1;%1$c%2$casprintf(&name,%4$cSully_%%d.c%4$c,i);%1$c%2$casprintf(&compile,%4$cclang -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d;%4$c,i,i);%1$c%2$casprintf(&cmd,%4$c./Sully_%%d;%4$c,i);%1$c%2$ckid = fopen(name,%4$cw%4$c);%1$c%2$cfprintf(kid,itself,10,9,i,34,itself);%1$c%2$cfclose(kid);%1$c%2$csystem(compile);%1$c%2$csystem(cmd);%1$c%2$cfree(compile), free(name), free(cmd);%1$c%2$ccompile = cmd = name = NULL;%1$c%2$creturn (0);%1$c}%1$c";
 	char	*name, *compile, *cmd;
 	FILE	*kid;
 
 	if (i == 0)
 		return (0);
-	asprintf(&name,"Sully_%d.c",i-1);
-	asprintf(&compile,"clang -Wall -Wextra -Werror Sully_%d.c -o Sully_%d;",i-1,i-1);
-	asprintf(&cmd,"./Sully_%d;",i-1);
+	i += (fopen("Sully_5.c", "r") == NULL) ? 0 : -1;
+	asprintf(&name,"Sully_%d.c",i);
+	asprintf(&compile,"clang -Wall -Wextra -Werror Sully_%d.c -o Sully_%d;",i,i);
+	asprintf(&cmd,"./Sully_%d;",i);
 	kid = fopen(name,"w");
-	fprintf(kid,itself,10,10,10,10,9,10,10,9,9,10,10,9,9,9,i-1,10,9,9,34,itself,34,10,9,9,10,9,9,10,10,9,10,9,9,10,9,34,34,10,9,34,34,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,10,9,10,9,10,9,10,10);
+	fprintf(kid,itself,10,9,i,34,itself);
 	fclose(kid);
 	system(compile);
 	system(cmd);
