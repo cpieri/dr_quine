2a8de4551fb24ebec44ee1f20ac83a3ffb38b032
Clément P <cpieri@student.42.fr>
2019-06-08 18:10:36 +0200

    add sully_5 py

--- Python/Sully/Sully.py
+++ Python/Sully/Sully.py
@@ -1,11 +1,11 @@
 # Writing Sully in Python3
-from os import system
+import os
 
 i = 5
 if i:
-	i -= 1
-	src = "# Writing Sully in Python3{0:c}from os import system{0:c}{0:c}i = {i}{0:c}if i:{0:c}{1:c}i -= 1{0:c}{1:c}src = {2:c}{msg}{2:c}{0:c}{1:c}kid = open('Sully_{3:c}i{4:c}.py'.format(i=i), 'w'){0:c}{1:c}kid.write(src.format(10, 9, 34, 123, 125, i=i, msg=src)){0:c}{1:c}kid.close(){0:c}{1:c}system('python3 Sully_{3:c}i{4:c}.py'.format(i=i)){0:c}"
+	i -= 1 if os.path.exists('Sully_5.py') else 0
+	src = "# Writing Sully in Python3{0:c}import os{0:c}{0:c}i = {i}{0:c}if i:{0:c}{1:c}i -= 1 if os.path.exists('Sully_5.py') else 0{0:c}{1:c}src = {2:c}{msg}{2:c}{0:c}{1:c}kid = open('Sully_{3:c}i{4:c}.py'.format(i=i), 'w'){0:c}{1:c}kid.write(src.format(10, 9, 34, 123, 125, i=i, msg=src)){0:c}{1:c}kid.close(){0:c}{1:c}os.system('python Sully_{3:c}i{4:c}.py'.format(i=i)){0:c}"
 	kid = open('Sully_{i}.py'.format(i=i), 'w')
 	kid.write(src.format(10, 9, 34, 123, 125, i=i, msg=src))
 	kid.close()
-	system('python3 Sully_{i}.py'.format(i=i))
+	os.system('python Sully_{i}.py'.format(i=i))
